1d39082fb4360c852bda5d0a7313329da1af51a4
dartal5 <5dartal@gmail.com>
2018-06-15 20:09:43 +0300

    local changes

--- app/src/Database/Database.php
+++ app/src/Database/Database.php
@@ -9,6 +9,14 @@ class Database
     private static $login = 'root';
     private static $pass = '';
 
+
+    private static $from_all = ' from car 
+                         inner join class on car.class_id = class.id 
+                         inner join transmission on car.transmission_id = transmission.id 
+                         inner join type on car.type_id = type.id
+                         inner join fuel on car.fuel_id = fuel.id
+                         cross join baseprice ';
+
     public static function close()
     {
         R::close();
@@ -22,15 +30,10 @@ class Database
     public static function getAllCars()
     {
         $res = R::getAll('select 
-                                car.id, car.make, car.model, car.info, car.status, car.pic as picture,
-                                class.type as class_type, transmission.type as tran_type, type.type as type, type.seats_from, type.seats_to, fuel.type as fuel_type,
-                                baseprice.coeff as base_coeff, class.coeff as class_coeff, transmission.coeff as transmission_coeff, type.coeff as type_coeff
-                                from car 
-                                inner join class on car.class_id = class.id 
-                                inner join transmission on car.transmission_id = transmission.id 
-                                inner join type on car.type_id = type.id
-                                inner join fuel on car.fuel_id = fuel.id
-                                cross join baseprice', []);
+                              car.id, car.make, car.model, car.info, car.status, car.pic as picture,
+                              class.type as class_type, transmission.type as tran_type, type.type as type, type.seats, fuel.type as fuel_type,
+                              baseprice.coeff as base_coeff, class.coeff as class_coeff, transmission.coeff as transmission_coeff, type.coeff as type_coeff'
+                              . Database::$from_all, []);
         foreach ($res as $key=>$val)
         {
             $res[$key]["price"] = $val["base_coeff"] * $val["class_coeff"] * $val["transmission_coeff"] * $val["type_coeff"];
@@ -42,16 +45,10 @@ class Database
     public static function getCar($id)
     {
         $res = R::getRow('select 
-                                car.id, car.make, car.model, car.info, car.status, car.pic as picture,
-                                class.type as class_type, transmission.type as tran_type, type.type as type, type.seats_from, type.seats_to, fuel.type as fuel_type,
-                                baseprice.coeff as base_coeff, class.coeff as class_coeff, transmission.coeff as transmission_coeff, type.coeff as type_coeff
-                                from car 
-                                inner join class on car.class_id = class.id 
-                                inner join transmission on car.transmission_id = transmission.id 
-                                inner join type on car.type_id = type.id
-                                inner join fuel on car.fuel_id = fuel.id
-                                cross join baseprice 
-                                where car.id = :id', [':id' => $id]);
+                              car.id, car.make, car.model, car.info, car.status, car.pic as picture,
+                              class.type as class_type, transmission.type as tran_type, type.type as type, type.seats_from, fuel.type as fuel_type,
+                              baseprice.coeff as base_coeff, class.coeff as class_coeff, transmission.coeff as transmission_coeff, type.coeff as type_coeff'
+                              . Database::$from_all . 'where car.id = :id', [':id' => $id]);
         $res["price"] = $res["base_coeff"] * $res["class_coeff"] * $res["transmission_coeff"] * $res["type_coeff"];
         return $res;
     }
@@ -59,14 +56,8 @@ class Database
     public static function getCarPriceCoeffs($id)
     {
         return R::getRow('select 
-                                car.status, baseprice.coeff as base_coeff, class.coeff as class_coeff, transmission.coeff as transmission_coeff, type.coeff as type_coeff
-                                from car 
-                                inner join class on car.class_id = class.id 
-                                inner join transmission on car.transmission_id = transmission.id 
-                                inner join type on car.type_id = type.id
-                                inner join fuel on car.fuel_id = fuel.id
-                                cross join baseprice 
-                                where car.id = :id', [':id' => $id]);
+                              car.status, baseprice.coeff as base_coeff, class.coeff as class_coeff, transmission.coeff as transmission_coeff, type.coeff as type_coeff'
+                              . Database::$from_all . '  where car.id = :id', [':id' => $id]);
     }
 
     public static function changeCarStatus($id, $status)
--- app/src/Main/Calculator/Calculator.php
+++ app/src/Main/Calculator/Calculator.php
@@ -14,15 +14,16 @@ class Calculator
     public function set(array $args)
     {
         $id = $args["id"];
-        $term = $args["term"];
+        $data_from = $args["data_from"];
+        $data_to = $args["data_to"];
 
-        if(!preg_match("(0|[1-9][0-9]*)", $term)) throw new \InvalidArgumentException("___ Wrong term " . $term . " ___");
+        if(valida) throw new \Exception("___ Wrong term " . $term . " ___");
 
         Db::connect();
         $data = Db::getCarPriceCoeffs($id);
 
-        if($data === false) throw new \InvalidArgumentException("___ Wrong id " . $id . " ___");
-        if($data["status"] == 0) throw new \InvalidArgumentException("___ Auto is unavaliable  ___");
+        if($data === false) throw new \Exception("___ Wrong id " . $id . " ___");
+        if($data["status"] == 0) throw new \Exception("___ Auto is unavaliable  ___");
 
         $this->data = $data;
         $this->data["id"] = $id;
