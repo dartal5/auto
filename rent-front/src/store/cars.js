ad946bca5ab1c5a5ef601737b5f78c7e2c25d29e
sashalavron <lavronsashka@gmail.com>
2018-06-16 20:36:22 +0300

    get all cars from back

--- rent-front/src/store/cars.js
+++ rent-front/src/store/cars.js
@@ -3,73 +3,79 @@ import axios from 'axios'
 export default {
     state: {
         cars: [
-            {
-              id: '1', 
-              make: 'BMW',
-              model: 'X6',
-              info: 'Тачка кууул',
-              status: 1,
-              picture: 'https://www.rubmw.ru/pictures/photoalbum/39261/large.jpg',
-              class_type: 'business',
-              train_type: 'auto',
-              type: 'sedan',
-              seats: 4,
-              fuel_type: 'petrol',
-              base_coeff: 400,
-              class_coeff: 2,
-              transmission_coeff: 1.6,
-              type_coeff: 1.7,
-              price: 2176 
-            },
-            {
-                id: '2', 
-                make: 'Lada',
-                model: 'Sedan',
-                info: 'Nu Takoe',
-                status: 1,
-                picture: 'https://www.rubmw.ru/pictures/photoalbum/39261/large.jpg',
-                class_type: 'business',
-                train_type: 'auto',
-                type: 'sedan',
-                seats: 4,
-                fuel_type: 'diesel',
-                base_coeff: 300,
-                class_coeff: 2,
-                transmission_coeff: 1.6,
-                type_coeff: 1.7,
-                price: 4176 
-            },
-            {
-                id: '3', 
-                make: 'Nissan',
-                model: 'Turbo',
-                info: 'This is very good car, buy it',
-                status: 1,
-                picture: 'https://www.rubmw.ru/pictures/photoalbum/39261/large.jpg',
-                class_type: 'sport',
-                train_type: 'auto',
-                type: 'sedan',
-                seats: 5,
-                fuel_type: 'petrol',
-                base_coeff: 400,
-                class_coeff: 2,
-                transmission_coeff: 1.6,
-                type_coeff: 1.7,
-                price: 2176 
-            },
+            // {
+            //   id: '1', 
+            //   make: 'BMW',
+            //   model: 'X6',
+            //   info: 'Тачка кууул',
+            //   status: 1,
+            //   picture: 'https://www.rubmw.ru/pictures/photoalbum/39261/large.jpg',
+            //   class_type: 'business',
+            //   train_type: 'auto',
+            //   type: 'sedan',
+            //   seats: 4,
+            //   fuel_type: 'petrol',
+            //   base_coeff: 400,
+            //   class_coeff: 2,
+            //   transmission_coeff: 1.6,
+            //   type_coeff: 1.7,
+            //   price: 2176 
+            // },
+            // {
+            //     id: '2', 
+            //     make: 'Lada',
+            //     model: 'Sedan',
+            //     info: 'Nu Takoe',
+            //     status: 1,
+            //     picture: 'https://www.rubmw.ru/pictures/photoalbum/39261/large.jpg',
+            //     class_type: 'business',
+            //     train_type: 'auto',
+            //     type: 'sedan',
+            //     seats: 4,
+            //     fuel_type: 'diesel',
+            //     base_coeff: 300,
+            //     class_coeff: 2,
+            //     transmission_coeff: 1.6,
+            //     type_coeff: 1.7,
+            //     price: 4176 
+            // },
+            // {
+            //     id: '3', 
+            //     make: 'Nissan',
+            //     model: 'Turbo',
+            //     info: 'This is very good car, buy it',
+            //     status: 1,
+            //     picture: 'https://www.rubmw.ru/pictures/photoalbum/39261/large.jpg',
+            //     class_type: 'sport',
+            //     train_type: 'auto',
+            //     type: 'sedan',
+            //     seats: 5,
+            //     fuel_type: 'petrol',
+            //     base_coeff: 400,
+            //     class_coeff: 2,
+            //     transmission_coeff: 1.6,
+            //     type_coeff: 1.7,
+            //     price: 2176 
+            // },
           ],
           paramsToShow: [
             'make', 'model', 'class_type', 'train_type', 'type', 'fuel_type', 'price'
           ]
     },
     mutations: {
-
+        setAllCars(state, payload) {
+            state.cars.push(payload)
+        }
     },
     actions: {
        getCars({commit}) {
             axios.get('http://localhost/app/public/index.php?action=getAllCars')
                  .then(response => {
-                     console.log(response)
+                     console.log(response.data)
+                     const cars = response.data
+                     for(let car in cars){
+                        commit('setAllCars', cars[car])
+                     }
                  })
                  .catch(error => {
                      console.log(error)
